ded1708aabfb37394f55758cc716ecc4426f3090
João Paulo Fachim Bosqueti <joaopaulo_fb@hotmail.com>
2025-12-11 17:10:35 -0300

    Enum - GetDisplayName

--- src/JotaSystem.Sdk.Common/Extensions/Enum/EnumDescriptionExtension.cs
+++ src/JotaSystem.Sdk.Common/Extensions/Enum/EnumDescriptionExtension.cs
@@ -1,4 +1,6 @@
 ﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
 
 namespace JotaSystem.Sdk.Common.Extensions.Enum
 {
@@ -7,6 +9,32 @@ namespace JotaSystem.Sdk.Common.Extensions.Enum
     /// </summary>
     public static class EnumDescriptionExtension
     {
+        /// <summary>
+        /// Retorna o nome amigável definido pelo atributo <see cref="DisplayAttribute"/> associado ao valor do enum.
+        /// Caso o atributo não esteja presente, tenta retornar a descrição definida em <see cref="DescriptionAttribute"/>.
+        /// Se nenhum atributo existir, retorna o nome do enum.
+        /// </summary>
+        /// <param name="value">Valor do enum.</param>
+        /// <returns>Nome definido em <see cref="DisplayAttribute"/>, descrição ou o nome do enum.</returns>
+        public static string GetDisplayName(this System.Enum value)
+        {
+            var field = value.GetType().GetField(value.ToString());
+            if (field == null) return value.ToString();
+
+            // DisplayAttribute
+            var displayAttribute = field.GetCustomAttribute<DisplayAttribute>();
+            if (displayAttribute != null && !string.IsNullOrWhiteSpace(displayAttribute.Name))
+                return displayAttribute.Name;
+
+            // DescriptionAttribute como fallback
+            var descriptionAttribute = field.GetCustomAttribute<DescriptionAttribute>();
+            if (descriptionAttribute != null)
+                return descriptionAttribute.Description;
+
+            // Nome do enum como último recurso
+            return value.ToString();
+        }
+
         /// <summary>
         /// Retorna a descrição associada ao valor do enum, definida pelo atributo <see cref="DescriptionAttribute"/>.
         /// Caso o atributo não esteja presente, retorna o nome do enum.
--- tests/JotaSystem.Sdk.Common.Tests/Extensions/Enum/EnumDescriptionExtensionTest.cs
+++ tests/JotaSystem.Sdk.Common.Tests/Extensions/Enum/EnumDescriptionExtensionTest.cs
@@ -1,5 +1,6 @@
 ﻿using JotaSystem.Sdk.Common.Extensions.Enum;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace JotaSystem.Sdk.Common.Tests.Extensions.Enum
 {
@@ -7,9 +8,29 @@ namespace JotaSystem.Sdk.Common.Tests.Extensions.Enum
     {
         private enum TestEnum
         {
+            [Display(Name = "Nome Exibido")]
+            Value,
             [Description("Descrição 1")]
             Value1,
             Value2
+        }
+
+        [Fact]
+        public void GetDisplayName_ShouldReturnDisplayName_WhenPresent()
+        {
+            Assert.Equal("Nome Exibido", TestEnum.Value.GetDisplayName());
+        }
+
+        [Fact]
+        public void GetDisplayName_ShouldReturnDescription_WhenDisplayNotPresent_ButDescriptionExists()
+        {
+            Assert.Equal("Descrição 1", TestEnum.Value1.GetDisplayName());
+        }
+
+        [Fact]
+        public void GetDisplayName_ShouldReturnEnumName_WhenNoAttribute()
+        {
+            Assert.Equal("Value2", TestEnum.Value2.GetDisplayName());
         }
 
         [Fact]
