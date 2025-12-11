0840476451362657ec42870a2a8dc14d95cfc50e
João Paulo Fachim Bosqueti <joaopaulo_fb@hotmail.com>
2025-12-11 15:24:48 -0300

    CsvHelper

---
+++ src/JotaSystem.Sdk.Common/Helpers/CsvHelper.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+namespace JotaSystem.Sdk.Common.Helpers
+{
+    public static class CsvHelper
+    {
+        public static string ExportToCsv<T>(IEnumerable<T> data, string separator = ";")
+        {
+            var properties = typeof(T).GetProperties();
+
+            var csv = new StringBuilder();
+
+            // Cabeçalho
+            csv.AppendLine(string.Join(separator, properties.Select(p => p.Name)));
+
+            // Linhas
+            foreach (var item in data)
+            {
+                var values = properties.Select(p =>
+                {
+                    var value = p.GetValue(item) ?? "";
+                    return value.ToString()!.Replace(separator, " ");
+                });
+
+                csv.AppendLine(string.Join(separator, values));
+            }
+
+            return csv.ToString();
+        }
+    }
+}
