d560c2170e36157be2e468638e04a3b6080d867f
João Paulo Fachim Bosqueti <joaopaulo_fb@hotmail.com>
2025-12-26 12:06:38 -0300

    ChatMessageModel

---
+++ src/JotaSystem.Sdk.Common/Enums/ChatRoleEnum.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JotaSystem.Sdk.Common.Enums
+{
+    public enum ChatRoleEnum
+    {
+        [Display(Name = "Você")]
+        User = 1,
+        [Display(Name = "Assistente de IA")]
+        Assistant = 2,
+        [Display(Name = "Sistema")]
+        System = 3
+    }
+}
---
+++ src/JotaSystem.Sdk.Common/Models/ChatMessageModel.cs
@@ -0,0 +1,12 @@
+﻿using JotaSystem.Sdk.Common.Enums;
+
+namespace JotaSystem.Sdk.Common.Models
+{
+    public class ChatMessageModel
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public ChatRoleEnum Role { get; set; }
+        public string Content { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
