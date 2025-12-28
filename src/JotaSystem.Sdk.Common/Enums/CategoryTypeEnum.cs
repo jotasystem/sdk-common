07a93c1e67a6b63237b197d0cba0b496c7217d1a
João Paulo Fachim Bosqueti <joaopaulo_fb@hotmail.com>
2025-12-28 11:45:44 -0300

    CategoryTypeEnum

---
+++ src/JotaSystem.Sdk.Common/Enums/CategoryTypeEnum.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JotaSystem.Sdk.Common.Enums
+{
+    public enum CategoryTypeEnum
+    {
+        [Display(Name = "Produto")]
+        Product = 1,
+        [Display(Name = "Serviço")]
+        Service = 2
+    }
+}
