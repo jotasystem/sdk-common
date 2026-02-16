11e0785f81f2cd6e86827209f8358d1bc4fd94b3
João Paulo Fachim Bosqueti <joaopaulo_fb@hotmail.com>
2026-02-15 22:32:40 -0300

    Enum - ReservationStatus

---
+++ src/JotaSystem.Sdk.Common/Enums/ReservationStatus.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JotaSystem.Sdk.Common.Enums
+{
+    public enum ReservationStatus
+    {
+        [Display(Name = "Ativo")]
+        Active = 1,
+        [Display(Name = "Lançado")]
+        Released = 2,
+        [Display(Name = "Consumido")]
+        Consumed = 3
+    }
+}
