b494f93f315e64a90f8151eefafbab5794886bab
João Paulo Fachim Bosqueti <joaopaulo_fb@hotmail.com>
2026-02-15 22:18:35 -0300

    Enum - InventoryMovement

---
+++ src/JotaSystem.Sdk.Common/Enums/InventoryMovementReason.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JotaSystem.Sdk.Common.Enums
+{
+    public enum InventoryMovementReason
+    {
+        [Display(Name = "Compra")]
+        Purchase = 1,
+        [Display(Name = "Venda")]
+        Sale = 2,
+        [Display(Name = "Devolução de Entrada")]
+        ReturnIn = 3,
+        [Display(Name = "Devolução de Saída")]
+        ReturnOut = 4,
+        [Display(Name = "Transferência de Entrada")]
+        TransferIn = 5,
+        [Display(Name = "Transferência de Saída")]
+        TransferOut = 6,
+        [Display(Name = "Ajuste")]
+        Adjustment = 7,
+        [Display(Name = "Reserva Consumida")]
+        ReservationConsumed = 8
+    }
+}
---
+++ src/JotaSystem.Sdk.Common/Enums/InventoryMovementType.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JotaSystem.Sdk.Common.Enums
+{
+    public enum InventoryMovementType
+    {
+        [Display(Name = "Entrada")]
+        In = 1,
+        [Display(Name = "Saída")]
+        Out = 2
+    }
+}
