299902fbd3a17f1ea5b146668859f77c1b08c3e9
João Paulo Fachim Bosqueti <joaopaulo_fb@hotmail.com>
2026-02-16 21:16:01 -0300

    Enum - AddressTypeEnum

---
+++ src/JotaSystem.Sdk.Common/Enums/AddressTypeEnum.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JotaSystem.Sdk.Common.Enums
+{
+    public enum AddressTypeEnum
+    {
+        [Display(Name = "Cobrança")]
+        Billing = 1,
+
+        [Display(Name = "Entrega")]
+        Shipping = 2,
+
+        [Display(Name = "Cobrança e Entrega")]
+        BillingAndShipping = 3,
+
+        [Display(Name = "Residencial")]
+        Residential = 4,
+
+        [Display(Name = "Comercial")]
+        Commercial = 5,
+
+        [Display(Name = "Outro")]
+        Other = 6
+    }
+}
