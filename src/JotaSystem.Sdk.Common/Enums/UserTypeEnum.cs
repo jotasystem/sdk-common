1157a14169e30e2fd420a623411304b3cb012a37
João Paulo Fachim Bosqueti <joaopaulo_fb@hotmail.com>
2026-03-07 09:16:42 -0300

    UserTypeEnum

---
+++ src/JotaSystem.Sdk.Common/Enums/UserTypeEnum.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JotaSystem.Sdk.Common.Enums
+{
+    public enum UserTypeEnum
+    {
+        [Display(Name = "Administrador")]
+        Admin = 1,
+        [Display(Name = "Colaborador")]
+        Employee = 2,
+        [Display(Name = "Cliente")]
+        Customer = 3,
+        [Display(Name = "Externo")]
+        External = 4
+    }
+}
