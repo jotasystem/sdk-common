3de1db6c3b80d754c73b1401dfcc07e7c97dcb15
João Paulo Fachim Bosqueti <joaopaulo_fb@hotmail.com>
2026-03-12 12:56:49 -0300

    OrderTypeEnum

---
+++ src/JotaSystem.Sdk.Common/Enums/OrderTypeEnum.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JotaSystem.Sdk.Common.Enums
+{
+    public enum OrderTypeEnum
+    {
+        [Display(Name = "Orçamento")]
+        Estimate = 1,
+        [Display(Name = "Pedido de Venda")]
+        SalesOrder = 2,
+        [Display(Name = "Serviço de Venda")]
+        SalesService = 3
+    }
+}
