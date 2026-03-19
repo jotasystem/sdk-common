d6aef2ffe19dd5f9e48984bb48c9b25d8caa4191
João Paulo Fachim Bosqueti <joaopaulo_fb@hotmail.com>
2026-03-19 17:52:20 -0300

    ProductTypeEnum & ServiceTypeEnum

---
+++ src/JotaSystem.Sdk.Common/Enums/ProductTypeEnum.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JotaSystem.Sdk.Common.Enums
+{
+    public enum ProductTypeEnum
+    {
+        [Display(Name = "Produto Simples")]
+        Simple = 1,
+        [Display(Name = "Produto Variável")]
+        Variable = 2,
+        [Display(Name = "Produto Composto")]
+        Composite = 3
+    }
+}
---
+++ src/JotaSystem.Sdk.Common/Enums/ServiceTypeEnum.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JotaSystem.Sdk.Common.Enums
+{
+    public enum ServiceTypeEnum
+    {
+        [Display(Name = "Serviço Simples")]
+        Simple = 1,
+        [Display(Name = "Pacote de Serviços")]
+        Package = 2
+    }
+}
