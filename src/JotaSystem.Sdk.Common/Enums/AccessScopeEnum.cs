7a133764db3b1b20f9f484cc7ef001e60fb81c3b
João Paulo Fachim Bosqueti <joaopaulo_fb@hotmail.com>
2026-03-22 14:20:05 -0300

    AccessScopeEnum

---
+++ src/JotaSystem.Sdk.Common/Enums/AccessScopeEnum.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JotaSystem.Sdk.Common.Enums
+{
+    public enum AccessScopeEnum
+    {
+        [Display(Name = "Qualquer")]
+        Any = 1,
+        [Display(Name = "Somente sede")]
+        HeadquartersOnly = 2,
+        [Display(Name = "Somente filial")]
+        BranchOnly = 3
+    }
+}
