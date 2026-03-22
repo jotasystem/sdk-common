30aaaad674ebb9322f153dabcbc153acb266f822
João Paulo Fachim Bosqueti <joaopaulo_fb@hotmail.com>
2026-03-22 11:43:22 -0300

    ClaimConstant

--- src/JotaSystem.Sdk.Common/Constants/ClaimConstant.cs
+++ src/JotaSystem.Sdk.Common/Constants/ClaimConstant.cs
@@ -3,17 +3,9 @@
     public static class ClaimConstant
     {
         // 🔹 Chaves de Claims
-        public const string UserId = "user_id";
         public const string UserType = "user_type";
-
-        public const string TenantId = "tenant_id";
-        public const string TenantLogo = "tenant_logo";
-        public const string TenantColor = "tenant_color";
-
-        public const string CurrentCompanyId = "current_company_id";
-        public const string DefaultCompanyId = "default_company_id";
-        public const string AllowedCompanyId = "allowed_company_id";
-
+        public const string UserId = "user_id";
+        public const string UserUuid = "user_uuid";
         public const string Username = "username";
         public const string Nickname = "nickname";
         public const string Email = "email";
@@ -21,9 +13,14 @@
         public const string Picture = "picture";
         public const string FullAccess = "full_access";
 
+        public const string TenantId = "tenant_id";
+        public const string CurrentCompanyId = "current_company_id";
+        public const string AllowedCompaniesId = "allowed_companies_id";
         public const string Culture = "culture";
         public const string TimeZone = "timezone";
         public const string Currency = "currency";
+        public const string Color = "color";
+        public const string Logo = "logo";
 
         public const string Roles = "roles";
         public const string Permissions = "permissions";
