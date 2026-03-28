cbea8b5eb0966c8a6aaee3827c0a05030c3c3a17
João Paulo Fachim Bosqueti <joaopaulo_fb@hotmail.com>
2026-03-28 14:16:48 -0300

    AuthorizationMode

---
+++ src/JotaSystem.Sdk.Common/Enums/AuthorizationModeEnum.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JotaSystem.Sdk.Common.Enums
+{
+    public enum AuthorizationMode
+    {
+        [Display(Name = "Nenhum")]
+        None = 0,
+        [Display(Name = "Permissão")]
+        Permission = 1,
+        [Display(Name = "Somente Administrador")]
+        AdminOnly = 2
+    }
+}
