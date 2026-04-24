c8c1ceb49107260fe454525d8bb561921af1b2ff
João Paulo Fachim Bosqueti <joaopaulo_fb@hotmail.com>
2026-04-24 14:55:20 -0300

    Ajuste CategoryTypeEnum

--- src/JotaSystem.Sdk.Common/Enums/CategoryTypeEnum.cs
+++ src/JotaSystem.Sdk.Common/Enums/CategoryTypeEnum.cs
@@ -4,8 +4,6 @@ namespace JotaSystem.Sdk.Common.Enums
 {
     public enum CategoryTypeEnum
     {
-        [Display(Name = "Nenhum")]
-        None = 0,
         [Display(Name = "Produto")]
         Product = 1,
         [Display(Name = "Serviço")]
