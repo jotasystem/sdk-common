60e7ec78c84bb3192078c7dcfc06b9c6308a1668
João Paulo Fachim Bosqueti <joaopaulo_fb@hotmail.com>
2026-05-22 11:52:55 -0300

    Enums AccountPlan e Bank

---
+++ src/JotaSystem.Sdk.Common/Enums/AccountPlanEnum.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JotaSystem.Sdk.Common.Enums
+{
+    public enum AccountPlanNatureEnum
+    {
+        [Display(Name = "Receita")]
+        Revenue = 1,
+        [Display(Name = "Custo")]
+        Cost = 2,
+        [Display(Name = "Despesa")]
+        Expense = 3,
+        [Display(Name = "Ativo")]
+        Asset = 4,
+        [Display(Name = "Passivo")]
+        Liability = 5,
+        [Display(Name = "Patrimônio Líquido")]
+        Equity = 6,
+        [Display(Name = "Dedução de Receita")]
+        RevenueDeduction = 7,
+        [Display(Name = "Receita Financeira")]
+        FinancialRevenue = 8,
+        [Display(Name = "Despesa Financeira")]
+        FinancialExpense = 9,
+        [Display(Name = "Tributo")]
+        Tax = 10
+    }
+}
---
+++ src/JotaSystem.Sdk.Common/Enums/BankEnum.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JotaSystem.Sdk.Common.Enums
+{
+    public enum BankReconciliationStatusEnum
+    {
+        [Display(Name = "Pendente")]
+        Pending = 1,
+        [Display(Name = "Conciliado")]
+        Reconciled = 2
+    }
+
+    public enum BankTransactionOriginEnum
+    {
+        [Display(Name = "Manual")]
+        Manual = 1,
+        [Display(Name = "Liquidação")]
+        Settlement = 2,
+        [Display(Name = "Importação")]
+        Import = 3,
+        [Display(Name = "Ajuste")]
+        Adjustment = 4
+    }
+
+    public enum BankTransactionStatusEnum
+    {
+        [Display(Name = "Confirmado")]
+        Confirmed = 1,
+        [Display(Name = "Pendente")]
+        Pending = 2,
+        [Display(Name = "Cancelado")]
+        Cancelled = 3
+    }
+
+    public enum BankTransactionTypeEnum
+    {
+        [Display(Name = "Entrada")]
+        Credit = 1,
+        [Display(Name = "Saída")]
+        Debit = 2
+    }
+}
