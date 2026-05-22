7372c75ebebfd736dad2208361ad7b31d6181118
João Paulo Fachim Bosqueti <joaopaulo_fb@hotmail.com>
2026-05-22 12:03:13 -0300

    Anuste AccountPlanNatureEnum

--- src/JotaSystem.Sdk.Common/Enums/AccountPlanEnum.cs
+++ src/JotaSystem.Sdk.Common/Enums/AccountPlanEnum.cs
@@ -4,25 +4,13 @@ namespace JotaSystem.Sdk.Common.Enums
 {
     public enum AccountPlanNatureEnum
     {
-        [Display(Name = "Receita")]
-        Revenue = 1,
-        [Display(Name = "Custo")]
-        Cost = 2,
-        [Display(Name = "Despesa")]
-        Expense = 3,
-        [Display(Name = "Ativo")]
-        Asset = 4,
-        [Display(Name = "Passivo")]
-        Liability = 5,
-        [Display(Name = "Patrimônio Líquido")]
-        Equity = 6,
-        [Display(Name = "Dedução de Receita")]
-        RevenueDeduction = 7,
         [Display(Name = "Receita Financeira")]
-        FinancialRevenue = 8,
+        Revenue = 1,
         [Display(Name = "Despesa Financeira")]
-        FinancialExpense = 9,
-        [Display(Name = "Tributo")]
-        Tax = 10
+        Expense = 2,
+        [Display(Name = "Deduções de Receita")]
+        RevenueDeduction = 3,
+        [Display(Name = "Impostos e Taxas")]
+        Tax = 4
     }
 }
